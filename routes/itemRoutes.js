2a957d9739d5f2ade1d29f37257bf587746b736b
William Luna <wluna98@gmail.com>
2022-09-24 19:36:32 -0700

    Init form pages added

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -7,9 +7,11 @@ router.route('/')
     .get(catchAsync(items.allItems)) //index all items
     .post(catchAsync(items.createItem)); // create item
 
+router.route('/create').get(items.createItemForm); //form to create item
+
 
 router.route('/:id')
-    .get(catchAsync(items.detailsItem))
+    .get(items.detailsItem)
     .patch(catchAsync(items.updateItem))
     .delete(catchAsync(items.deleteItem));
 
