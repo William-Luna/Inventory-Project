ada646c7771b3bdd9717a206b0c666c9db1ef181
William Luna <wluna98@gmail.com>
2022-10-11 01:06:20 -0700

    Tailwind Implementation, Navbar and footer added

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,ejs,css}",
+    "./views/**/*.ejs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
